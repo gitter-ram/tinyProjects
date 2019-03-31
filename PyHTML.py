8317111d0640a53d3f2f79e4df2c61752794df1e
Gitter <40431840+gitter-ram@users.noreply.github.com>
2019-03-31 19:22:48 +0530

    Create PyHTML.py
    An HTML Generator.

---
+++ PyHTML.py
@@ -0,0 +1,7 @@
+TITLE = None
+KEYWORDS = None
+DESCRIPTION = None
+OFILE = None
+def print-header():
+    print("<!---Machine Generated--->", outfile = OFILE)
+    print("<html lang="en">", outfile = OFILE)
